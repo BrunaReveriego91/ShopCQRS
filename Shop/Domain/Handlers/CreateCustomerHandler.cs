2b543e54d22ad815b30c20da6203df41c02a1666
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2022-03-24 11:39:12 -0300

    Add project files.

---
+++ Shop/Domain/Handlers/CreateCustomerHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Shop.Domain.Commands.Requests;
+using Shop.Domain.Commands.Responses;
+
+namespace Shop.Domain.Handlers
+{
+    public class CreateCustomerHandler : IRequestHandler<CreateCustomerRequest, CreateCustomerResponse>
+    {
+
+        public Task<CreateCustomerResponse> Handle(CreateCustomerRequest request, CancellationToken cancellationToken)
+        {
+            //Verifica se o cliente já está cadastrado
+
+            //Valida os dados
+            //Insere o cliente
+            //Envia e-mail de boas vindas
+
+            var result = new CreateCustomerResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = "José Mojica",
+                Email = "zedocaixa@gmail.com",
+                Date = DateTime.Now
+            };
+
+            return Task.FromResult(result);
+
+        }
+    }
+}
